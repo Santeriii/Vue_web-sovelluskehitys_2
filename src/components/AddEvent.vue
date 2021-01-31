ad1d78d17365d334d92905e5197ba8ccd6c02432
Santeri Einola <einola.santeri@gmail.com>
2021-01-31 18:01:36 +0200

    Tehtävä 6 suoritettu

--- src/components/AddEvent.vue
+++ src/components/AddEvent.vue
@@ -1,11 +1,41 @@
 <template>
+<div>
     <p class="homeText">
         Syötä tapahtuma
     </p>
+    <br/>
+    <input type="text" v-model="postEvent" @change="postPost()">
+    <ul v-if="errors && errors.length">
+        <li v-for="error of errors" :key="error.message">
+            {{ error.message }}
+        </li>
+    </ul>
+</div>
 </template>
 <script>
+import axios from 'axios'
+
     export default {
-    name: 'AddEvent'
+    name: 'AddEvent',
+
+    data() {
+        return {
+            postEvent: '',
+            errors: []
+            }
+        },
+
+    methods: {
+        postPost() {
+            axios.post(`http://localhost:3001/notes`, {
+                content: this.postEvent,
+                important: false
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+        }
+    }
 };
 </script>
 <style scoped>
